b3286ec43d8a88256cadf5f80d1d95fc546781c2
nkp007 <neilkpatel@gmail.com>
2017-07-03 17:51:20 -0400

    first commit

---
+++ partyRock app/MainVC.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  partyRock app
+//
+//  Created by Neil Patel on 7/3/17.
+//  Copyright © 2017 Neil Patel. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
+
+// prepare(for segue --> AnyObject = Any
+// Errors: segue.destination (don't include view controller)
+// get up to date version of didFinishLaunching ---> return true
+
